7de412aec6d19e7f58bdcc0516c0f38de5495210
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-05-19 07:29:40 -0500

    Se agrega formulario

--- src/index.js
+++ src/index.js
@@ -3,17 +3,13 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 
 import "./global.css";
-import Badge from "./components/Badge";
+import BadgeNew from "./pages/BadgeNew";
 
 const container = document.getElementById("app");
 
 ReactDOM.render(
-  <Badge
-    firstName="Marco"
-    lastName="Elizalde"
-    jobTitle="Backend Engineer"
-    twitter="marcoETmx"
-    avatarUrls="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
+  <BadgeNew
+ 
   />,
   container
 );
