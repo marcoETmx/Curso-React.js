a51c088fae2599c779cb1d74352a17b10fa9c47b
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-05-19 06:42:22 -0500

    props

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
 
-import './global.css';
-import Badge from './components/Badge';
+import "./global.css";
+import Badge from "./components/Badge";
 
-const container = document.getElementById('app');
+const container = document.getElementById("app");
 
-ReactDOM.render(<Badge />, container);
+ReactDOM.render(
+  <Badge
+    firstName="Marco"
+    lastName="Elizalde"
+    jobTitle="Backend Engineer"
+    twitter="marcoETmx"
+    avatarUrls="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
+  />,
+  container
+);
